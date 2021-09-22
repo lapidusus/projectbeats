5f950856516d7a5b63f50c39823fd68e48ece87a
Stas Golub <79.25@bk.ru>
2021-09-22 23:31:38 +0500

    правки от себя

--- products.js
+++ products.js
@@ -2,18 +2,33 @@ const mesureWidth = () => {
     return 500;
 }
 
-const openItem = item => {
+const closeEveryItemInContainer = (container) => {
+    const items = container.find(".products-menu__item");
+    const content = container.find(".products-menu__content");
+
+    items.removeCLass("active");
+    content.width(0);
+}
+
+const openItem = (item) => {
     const hiddenContent = item.find(".products-menu__content");
     const reqWidth = mesureWidth();
 
+    item.addClass("active");
     hiddenContent.width(reqWidth); 
 }
 
-$(".products-menu__title").on("click", e => {
+$(".products-menu__title").on("click", (e) => {
     e.preventDefault();
 
     const $this = $(e.currentTarget);
     const item = $this.closest(".products-menu__item");
+    const itemOpened = item.hasClass("active");
+    const container = $this.closest(".products-menu");
 
-    openItem(item);
-})
+    if (itemOpened) {
+        closeEveryItemInContainer(container)
+    } else {
+        openItem(item); 
+    }
+});
